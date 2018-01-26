3cc09fbd4eda1920cd6cfcdaa54d9413e68b9fa5
Stefan <stefankrempel@gmail.com>
2018-01-26 15:44:21 +0100

    moved conv_concat, mlp_concat and init_weight functions to separate file layers.py; used these functions in generator.py

--- discriminators.py
+++ discriminators.py
@@ -7,91 +7,10 @@ import torch.nn as nn
 from torch.autograd import Variable
 from torch.nn.utils import weight_norm as wn    # import weight_norm
 
+from layers import mlp_concat, conv_concat, init_weights
+
 import logging
 
-
-### static functions ###
-
-def conv_concat(x, y, num_cls):
-    dim_y = len(y.size())
-    bs = y.size(0)
-
-    if dim_y == 1:
-        # y = T.extra_ops.to_one_hot(y, self.num_cls)
-        # maybe need somewhere to transform to variable
-        label = torch.zeros((bs, num_cls))  # zero tensor
-        if y.is_cuda:
-            label = label.cuda()
-        # label = Variable(label) ??
-        y = label.scatter(1, y.data, 1)  # set label to 1 at the indices specified by y --> 1-hot-encoding
-
-    if dim_y == 2:
-        # y = y.dimshuffle(0, 1, 'x', 'x')
-        y = y.unsqueeze(2).unsqueeze(3)
-
-    assert dim_y == 4, 'Dimension of y != 4'
-
-    # T.concatenate([x, y*T.ones((x.shape[0], y.shape[1], x.shape[2], x.shape[3]))], axis=1)
-    y = y * Variable(torch.ones((x.size(0), y.size(1), x.size(2), x.size(3))))
-    return torch.cat([x, y], dim=1)
-
-
-def mlp_concat(x, y, num_cls):
-    dim_y = len(y.size())
-    bs = y.size(0)
-
-    if dim_y == 1:
-        # y = T.extra_ops.to_one_hot(y, self.num_cls)
-        # maybe need somewhere to transform to variable
-        label = torch.zeros((bs, num_cls))  # zero tensor
-        if y.is_cuda:
-            label = label.cuda()
-        # label = Variable(label) ??
-        y = label.scatter(1, y.data, 1)  # set label to 1 at the indices specified by y --> 1-hot-encoding
-
-    assert dim_y == 2, 'Dimension of y != 2'
-
-    return torch.cat([x, y], dim=1)
-
-
-def init_weights(model, normal=None):
-    '''initializes the weights of specific NN layers by xavier normal initialization
-
-    Args:
-        model (torch.nn.Module): neural net model
-        normal (list): (optional) params list for standard normal initialization, [mu, sigma]
-
-    '''
-
-    import warnings
-
-    if normal:
-        mu = normal[0]
-        sigma = normal[1]
-
-    if type(model) in [nn.Linear]:  # linear layers
-        if normal:
-            model.weight.data.normal_(mu, sigma)
-            model.bias.data.fill_(0)
-        else:
-            nn.init.xavier_normal(model.weight.data)
-    elif type(model) in [nn.ConvTranspose2d, nn.Conv2d]:  # convolutional layers
-        if normal:
-            model.weight.data.normal_(mu, sigma)
-            model.bias.data.fill_(0)
-        else:
-            nn.init.xavier_normal(model.weight.data)
-    elif type(model) in [nn.BatchNorm2d]:   # init batch normalizations from normal dist
-        if normal:
-            model.weight.data.normal_(mu, sigma)
-            model.bias.data.fill_(0)
-        else:
-            model.weight.data.normal_(1.0, 0.02)
-            model.bias.data.fill_(0)
-    else:
-        warnings.warn('Initialization failed. Unknown module type: \n' + str(type(model)), UserWarning)
-    print('Weights initialized.')
-        
 
 ### discriminators ###
 
--- generator.py
+++ generator.py
@@ -2,6 +2,9 @@ import torch
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
+from torch.nn.utils import weight_norm as wn    # import weight_norm
+
+from layers import conv_concat, mlp_concat
 
 
 def read_data():
@@ -19,14 +22,14 @@ def read_data():
     return trainloader
 
 
-batch_size = 1
-
-
 class Generator(nn.Module):
-    def __init__(self, input_size=101, dense_neurons=8192,
+    def __init__(self, input_size=101, num_classes=10,
+                 dense_neurons=8192,
                  num_output_features=3072):
 
         super(Generator, self).__init__()
+
+        self.num_classes = num_classes
 
         self.Dense = nn.Linear(input_size, dense_neurons)
         self.Relu = nn.ReLU()
@@ -45,39 +48,30 @@ class Generator(nn.Module):
         self.BatchNorm2D_1 = nn.BatchNorm2d(dense_neurons * 4)
         self.BatchNorm2D_2 = nn.BatchNorm2d(num_output_features)
 
-    def mlp_concat(self, z, y):
-        return z + y
-
-    def conv_concat_layer(self, x, y):
-        return x + y
-
-    def weigth_norm(self, x):
-        return x
-
     def forward(self, z, y):
-        x = self.mlp_concat(z, y)
+        x = mlp_concat(z, y, self.num_classes)
 
         x1 = self.Dense(x)
         x1 = self.Relu(x1)
         x1 = self.BatchNorm1D(x1)
 
         x2 = x1.resize_(-1, 512, 4, 4)
-        x2 = self.conv_concat_layer(x2, y)
+        x2 = conv_concat(x2, y, self.num_classes)
 
-        x3 = self.Deconv2D_0(x2)                # output shape (256,8,8) = 8192 * 2
+        x3 = self.Deconv2D_0(x2)                    # output shape (256,8,8) = 8192 * 2
         x3 = self.Relu(x3)
         x3 = self.BatchNorm2D_0(x3)
 
-        x4 = self.conv_concat_layer(x3, y)
+        x4 = conv_concat(x3, y, self.num_classes)
 
-        x5 = self.Deconv2D_1(x4)                # output shape (128,16,16) = 8192 * 2 * 2
+        x5 = self.Deconv2D_1(x4)                    # output shape (128,16,16) = 8192 * 2 * 2
         x5 = self.Relu(x5)
         x5 = self.BatchNorm2D_1(x5)
 
-        x6 = self.conv_concat_layer(x5, y)
-        x6 = self.Deconv2D_2(x6)                # output shape (3, 32, 32) = 3072
+        x6 = conv_concat(x5, y, self.num_classes)
+        x6 = self.Deconv2D_2(x6)                    # output shape (3, 32, 32) = 3072
         x6 = self.Tanh(x6)
 
-        x7 = self.weigth_norm(x6)
+        x7 = wn(x6)
 
         return x7
